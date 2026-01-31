18d85696d6e13c98b8d8c55e6da2e074b273e5c4
Ali Ibne Imran <132182709+aliibneimran@users.noreply.github.com>
2026-02-01 01:20:15 +0600

    update

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL,
+        origin: 'https://chat.sohodorit.com',
         methods: ['GET', 'POST'],
     },
 });
